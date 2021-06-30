c83562a8518347754d393bac0a6335d7dc47a061
Brandyn Amado <userid788@gmail.com>
2021-06-30 14:09:40 -0400

    Setup elements page w/ file structure & routing, added simple button element

--- vera-frontend/src/App.js
+++ vera-frontend/src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Elements from "./ElementsPage/Elements";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <div>
+            <Link to="/">Landing</Link>{' '}
+            <Link to="/elements">Elements</Link>{' '}
+          </div>
+          <hr/><br/>
+          <div>
+            <Switch>
+              <Route exact path="/">
+              </Route>
+              <Route path="/elements">
+                <Elements />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </header>
     </div>
   );
