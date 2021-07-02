af8d45ab0bb69b7640cf7e41b0f2af45c0f75206
Brandyn Amado <userid788@gmail.com>
2021-07-01 23:51:52 -0400

    Committing additional changes meant for last commit

--- vera-frontend/src/ElementsPage/Elements.js
+++ vera-frontend/src/ElementsPage/Elements.js
@@ -1,6 +1,8 @@
 import Buttons from './components/Buttons';
 import Navbars from './components/Navbars';
-import SongCard from "./components/SongCard";
+import SongCard from './components/SongCard';
+import Dropdowns from './components/Dropdown';
+import './ElementsPage.css';
 
 var songcardProps = {
     "title": "Test Title",
@@ -9,12 +11,18 @@ var songcardProps = {
     "listens": "100"
 }
 
+var dropdownProps = {
+    "header": "What’s a NFT (non-fungible token)?",
+    "body": "Take the painting of the Mona Lisa, for example. It is copied, photographed, and distributed everywhere, but there's only one original physical painting. This gives the original painting and its owner incredible value due to its rarity. Non-Fungible Tokens do this for digital creations like art, games, and in our case, music."
+}
+
 function Elements() {
     return (
-        <div>
-            <Navbars />
-            <Buttons />
-            <SongCard song={songcardProps}/>
+        <div className="elements-container">
+            <Navbars /><br />
+            <Buttons /><br /><br />
+            <Dropdowns content={dropdownProps}/><br/>
+            <SongCard song={songcardProps}/><br />
         </div>
     );
 }
