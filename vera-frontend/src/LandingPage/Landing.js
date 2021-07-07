308b15fb539ad26f04bb6a8c8807f693485b6c03
Jeff Xu <jiaxijeffxu@gmail.com>
2021-07-07 14:52:58 -0700

    create features section

--- vera-frontend/src/LandingPage/Landing.js
+++ vera-frontend/src/LandingPage/Landing.js
@@ -2,6 +2,7 @@ import './styles/Landing.css';
 import Navbars from '../ElementsPage/components/Navbars';
 import Header from './components/Header';
 import GettingStarted from './components/GettingStarted';
+import Features from './components/Features';
 
 function Landing() {
     return (
@@ -9,6 +10,7 @@ function Landing() {
             <Navbars />
             <Header />
             <GettingStarted />
+            <Features />
         </div>
     );
 }
