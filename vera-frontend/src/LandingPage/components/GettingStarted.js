27c041531b17840333ad9e59d4e2677ada5d1365
Jeff Xu <jiaxijeffxu@gmail.com>
2021-07-12 20:30:29 -0700

    getting started mobile view

--- vera-frontend/src/LandingPage/components/GettingStarted.js
+++ vera-frontend/src/LandingPage/components/GettingStarted.js
@@ -5,28 +5,76 @@ import {
     Image
 } from 'react-bootstrap';
 import '../styles/GettingStarted.css';
-import laptopIcon from '../styles/images/laptop-icon.png';
+import laptopIcon from '../styles/images/laptop-icon.svg';
+import investIcon from '../styles/images/invest-icon.svg';
+import tradeIcon from '../styles/images/trade-icon.svg';
+import investmentIcon from '../styles/images/investment-icon.svg';
 
 function GettingStarted() {
     return (
         <Container fluid className="gettingstarted-container">
-            <Row>
+            <Row className="gettingstarted-desktop">
                 <Col className="gettingstarted-col">
                     <Image className="gettingstarted-image" src={laptopIcon} />
+                    <Image className="gettingstarted-image" src={investmentIcon} />
                 </Col>
                 <Col>
-                    <p1 className="gettingstarted-header">1.  Sign Up</p1>
+                    <p className="gettingstarted-header">1.  Sign Up</p>
                     <p className="gettingstarted-text">
                         Sign up for an account &<br/>
                         customize your feed 
                     </p>
-                    <p1 className="gettingstarted-header">2.  Invest</p1>
+                    <p className="gettingstarted-header">2.  Invest</p>
                     <p className="gettingstarted-text">
                         Invest in musicians<br/>
                         by purchasing NFTs
                     </p>
+                    <p className="gettingstarted-header">3.  Grow</p>
+                    <p className="gettingstarted-text">
+                        Grow your investments<br/>
+                        as they grow their fan base
+                    </p>
+                    <p className="gettingstarted-header">4.  Trade</p>
+                    <p className="gettingstarted-text">
+                        Trade your NFTs &<br/>
+                        expand your portfolio
+                    </p>
                 </Col>
             </Row>
+            <div className="gettingstarted-mobile">
+                <div className="gettingstarted-mobile-icon-group">
+                    <p className="gettingstarted-header">1.  Sign Up</p>
+                    <Image className="gettingstarted-image" src={laptopIcon} />
+                    <p className="gettingstarted-text">
+                        Sign up for an account &<br/>
+                        customize your feed 
+                    </p>
+                </div>
+                <div className="gettingstarted-mobile-icon-group">
+                    <p className="gettingstarted-header">2.  Invest</p>
+                    <Image className="gettingstarted-image" src={investIcon} />
+                    <p className="gettingstarted-text">
+                        Invest in musicians<br/>
+                        by purchasing NFTs
+                    </p>
+                </div>
+                <div className="gettingstarted-mobile-icon-group">
+                    <p className="gettingstarted-header">3.  Grow</p>
+                    <Image className="gettingstarted-image" src={investmentIcon} />
+                    <p className="gettingstarted-text">
+                        Grow your investments<br/>
+                        as they grow their fan base 
+                    </p>
+                </div>
+                <div className="gettingstarted-mobile-icon-group">
+                    <p className="gettingstarted-header">4.  Trade</p>
+                    <Image className="gettingstarted-image" src={tradeIcon} />
+                    <p className="gettingstarted-text">
+                        Trade your NFTs &<br/>
+                        expand your portfolio
+                    </p>
+                </div>
+            </div>
         </Container>
     );
 }
