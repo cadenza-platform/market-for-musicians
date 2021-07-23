1ab205e2fd54db3df1cc9c00d1b5ebb6df09b099
Brandyn Amado <userid788@gmail.com>
2021-07-23 12:41:25 -0400

    New artist page content (icons not updated yet)

--- vera-frontend/src/LandingPage/components/ArtistFeatures.js
+++ vera-frontend/src/LandingPage/components/ArtistFeatures.js
@@ -20,10 +20,10 @@ function ArtistFeatures() {
                 </Col>
                 <Col className="artist-feature-description">
                     <h2 className="artist-feature-title">
-                        Crowd Sourced Revenue
+                        How does Vera help you?
                     </h2>
                     <p className="artist-feature-body">
-                        Vera allows you to unlock a new stream of revenue by tapping into your fanbase for trusted investment. By distributing assets such as music and special content to the public, you can provide an unparalleled level of interaction with your fans.
+                        Vera allows you to unlock a new stream of crowd-funded revenue by tapping into your fanbase for trusted investment. By distributing assets such as music and special content to the public, you can provide an unparalleled level of interaction with your fans while bringing in a new source of income to keep the music flowing.
                     </p>
                 </Col>
             </Row>
@@ -33,10 +33,10 @@ function ArtistFeatures() {
                 </Col>
                 <Col className="artist-feature-description">
                     <h2 className="artist-feature-title">
-                        Creative Freedom
+                        Do I retain creative freedom and ownership over my work?
                     </h2>
                     <p className="artist-feature-body">
-                        Our goal at Vera is to preserve your creative freedom while creating new opportunities using our crowd funding model. We want you to keep making music that we all love, and we’re here to support you along the way.
+                        Our goal at Vera is to preserve your creative freedom while creating new opportunities using our crowd funding model. You retain all ownership and copyright over your original work, while your fans gain ownership of a limited edition DVR of your music. We want you to keep making music that we all love, and we’re here to support you along the way.
                     </p>
                 </Col>
             </Row>
@@ -46,10 +46,10 @@ function ArtistFeatures() {
                 </Col>
                 <Col className="artist-feature-description">
                     <h2 className="artist-feature-title">
-                        Visual Analytics
+                        How do I create a DVR (digital vinyl record)?
                     </h2>
                     <p className="artist-feature-body">
-                        Vera provides detailed analytics and performance details of DVRs that you have distributed to the public. Our straightforward interface allows you to track user engagement in addition to transactional information on your DVRs.
+                        Once you have a song on a streaming service such as Spotify, creating a Vera DVR is as simple as clicking a button. You’ll simply connect your account and enter in information about the piece that will add to the creative value of the DVR! Vera will automatically set up an initial DVR auction for your followers and fans on the site.
                     </p>
                 </Col>
             </Row>
@@ -59,10 +59,36 @@ function ArtistFeatures() {
                 </Col>
                 <Col className="artist-feature-description">
                     <h2 className="artist-feature-title">
-                        Flexible Exchange
+                        How do I monetize my DVRs?
                     </h2>
                     <p className="artist-feature-body">
-                        Vera’s flexible exchange is a secure and regulated marketplace in which your DVRs can be transacted in a safe and efficient manner.
+                        You have the flexibility to set monetization rates on sales of your digital vinyl records! You make both a large percentage of initial record auction sales, plus a small commission on all future transactions of your records on the Vera public market.
+                    </p>
+                </Col>
+            </Row>
+            <Row className="artist-feature-row">
+                <Col>
+                    <Image src={recommendationsIcon} className="artist-feature-icon"/>
+                </Col>
+                <Col className="artist-feature-description">
+                    <h2 className="artist-feature-title">
+                        Security
+                    </h2>
+                    <p className="artist-feature-body">
+                        Vera’s flexible exchange is a secure and regulated marketplace in which your DVRs can be transacted in a safe and efficient manner. Our thorough verification and certification processes for users and content on the platform allow us to ensure and maintain authenticity in our markets.
+                    </p>
+                </Col>
+            </Row>
+            <Row className="artist-feature-row">
+                <Col>
+                    <Image src={recommendationsIcon} className="artist-feature-icon"/> 
+                </Col>
+                <Col className="artist-feature-description">
+                    <h2 className="artist-feature-title">
+                        Low Environmental Impact
+                    </h2>
+                    <p className="artist-feature-body">
+                        Traditional NFTs and other new, online art market systems utilize high-energy processes that use extensive amounts of electricity. Vera is committed to a more environmentally friendly approach to decentralized market creation and helping reduce the carbon footprint of the internet.
                     </p>
                 </Col>
             </Row>
